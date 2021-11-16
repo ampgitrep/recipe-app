9762574d7f6c1a5471dd9bec3f9dd3e31c668665
ampgitrep <apulvers@my.chemeketa.edu>
2021-11-15 21:26:37 -0800

    added random select and onclick handler to weekly recipe

--- src/WeeklyRecipes.js
+++ src/WeeklyRecipes.js
@@ -4,24 +4,106 @@ import RecipeControl from "./RecipeControl";
 
 const fakeDatabase = [{
     id: 0,
-    image:"blob:http://localhost:3000/9215c2fa-1a3f-4c75-8758-c725165950ba",
+    name: "test1",
+    ingredients: ["4 cups tests",],
+    image: "blob:http://localhost:3000/9215c2fa-1a3f-4c75-8758-c725165950ba",
+    instructions: [{ id: 1, instruction: "blah blah" }, { id: 2, instruction: "test blah" },],
+    rating: 4,
+}, {
+    id: 1,
+    name: "test2",
+    image: "blob:http://localhost:3000/9215c2fa-1a3f-4c75-8758-c725165950ba",
+    ingredients: ["4 cups tests",],
+    instructions: [{ id: 1, instruction: "blah blah" }, { id: 2, instruction: "test blah" },],
+    rating: 3,
+}, {
+    id: 2,
+    name: "test3",
+    image: "blob:http://localhost:3000/9215c2fa-1a3f-4c75-8758-c725165950ba",
+    ingredients: ["4 cups tests",],
+    instructions: [{ id: 1, instruction: "blah blah" }, { id: 2, instruction: "test blah" },],
+    rating: 5,
+}, {
+    id: 3,
+    name: "test4",
+    image: "blob:http://localhost:3000/9215c2fa-1a3f-4c75-8758-c725165950ba",
+    ingredients: ["4 cups tests",],
+    instructions: [{ id: 1, instruction: "blah blah" }, { id: 2, instruction: "test blah" },],
+    rating: 2,
+}, {
+    id: 4,
+    name: "test5",
+    image: "blob:http://localhost:3000/9215c2fa-1a3f-4c75-8758-c725165950ba",
+    ingredients: ["4 cups tests",],
+    instructions: [{ id: 1, instruction: "blah blah" }, { id: 2, instruction: "test blah" },],
+    rating: 3,
+}, {
+    id: 5,
+    name: "test6",
+    image: "blob:http://localhost:3000/9215c2fa-1a3f-4c75-8758-c725165950ba",
+    ingredients: ["4 cups tests",],
+    instructions: [{ id: 1, instruction: "blah blah" }, { id: 2, instruction: "test blah" },],
+    rating: 5,
+}, {
+    id: 6,
+    name: "test7",
+    image: "blob:http://localhost:3000/9215c2fa-1a3f-4c75-8758-c725165950ba",
+    ingredients: ["4 cups tests",],
+    instructions: [{ id: 1, instruction: "blah blah" }, { id: 2, instruction: "test blah" },],
+    rating: 2,
+}, {
+    id: 7,
+    name: "test8",
+    image: "blob:http://localhost:3000/9215c2fa-1a3f-4c75-8758-c725165950ba",
     ingredients: ["4 cups tests",],
     instructions: [{ id: 1, instruction: "blah blah" }, { id: 2, instruction: "test blah" },],
     rating: 4,
-},]
+}]
 
 
-const WeeklyRecipes = ({recipes}) => {
+const WeeklyRecipes = ({ }) => {
+    const [weekList, setWeekList] = useState([])
 
-    //we can store our weekly recipes in this array
-    const [recipeList, setRecipeList] = useState();
-        //Wont work until RecipeList is importing recipes from a database. Right now Recipelists "recipe" is undefined because it isn't given anything
+    const handleClick = () => {
+        const recipeArray = [];
+        for (let i = 1; i <= 5; i++) {
+            let randNum = Math.floor((Math.random(fakeDatabase.length) * fakeDatabase.length - 1) + 1);
+            recipeArray.push(fakeDatabase[randNum])
+        }
+        setWeekList(recipeArray);
+    }
+    const addToList = (id) => {
+        const currentArray = [...weekList];
+        if (currentArray.length <= 5) {
+            currentArray.push(fakeDatabase[id]);
+            setWeekList(currentArray);
+        }
+        if (currentArray.length > 5) {
+            alert("5 recipes have already been selected for the week.")
+        }
+    }
+
+    console.log(weekList);
     return (
-        <div>
-            <p>Pick your recipes for the week, or pick them at random</p>
-            
-            <Link to="/AddRecipes" element={<RecipeControl/>}>Add New Recipe </Link>
-        <Link to="/WeeklyRecipes" element={<WeeklyRecipes/>}>Pick your weekly recipes</Link>
+        <div style={{
+            color: "#FFFFFF",
+            backgroundColor: "black"
+        }}>
+            <ul>
+                <p style={{ color: "#FFFFFF", }}>Pick your recipes for the week, or pick them at random</p>
+                {fakeDatabase.map(({ rating, name, id, image }) => (
+                    <li key={id}
+                        style={{
+                            width: "20%",
+                            height: "20%"
+                        }}
+                        onClick={() => addToList(id)}>{name} - {rating} stars</li>
+                ))}
+            </ul>
+            <button onClick={handleClick}>pick recipes for me</button>
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            <Link to="/AddRecipes" element={<RecipeControl />}>Add New Recipe </Link>
+            <Link to="/WeeklyRecipes" element={<WeeklyRecipes />}>Pick your weekly recipes</Link>
         </div>
     )
 }
