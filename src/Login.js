803d9484284d0449a9514e12992d36e3fa989314
ampgitrep <apulvers@my.chemeketa.edu>
2021-11-28 12:55:15 -0800

    added final functionality to shoppinglist function added placeholder for dom element

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Container, Columns } from "react-bulma-components"
+
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
